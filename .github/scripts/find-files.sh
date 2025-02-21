91936a892e92def6dfbd9a6be902f2bc4d7b8834
Melissa Garcia <ens15038@adobe.com>
2025-02-21 09:29:46 -0800

    pass function param

--- .github/scripts/find-files.sh
+++ .github/scripts/find-files.sh
@@ -1,5 +1,5 @@
-root="./src/pages"
 func=$1
+root="./src/pages"
 
 # TODO: may want to only find certain types of files
 find "${root}" -type f \( -name "*.md" -o -name "*.json" \) -exec echo "{}" \; | while read i; do func $i; done
