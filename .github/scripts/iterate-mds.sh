f3f4a086dca3789091d7d257427a9fa7e4651c0b
Melissa Garcia <ens15038@adobe.com>
2025-02-21 11:20:18 -0800

    wip

--- .github/scripts/iterate-mds.sh
+++ .github/scripts/iterate-mds.sh
@@ -31,20 +31,9 @@ process()
     filename=$1
 
     path="${path_prefix}${filename#$root/}"
-    url="${home}/preview/${org}/${site}/${branch}${path}"
+    url="${home}/${action}/${org}/${site}/${branch}${path}"
 
-    case "$action" in
-        "cache")
-            echo "TODO - cache ${url}" 
-            echo "2a" 
-            ;;
-        "preview")
-            echo "TODO - preview ${url}" 
-            echo "2b"
-            ;;
-        *)
-            fail "Unknown action" ;;
-    esac
+    echo "${url}"
 }
 
 action=$1
@@ -56,11 +45,22 @@ branch="main"
 path_prefix="/github-actions-test/"
 root="../../src/pages"
 
+case "$action" in
+    cache | preview)
+        ;;
+    *)
+        fail "Unknown action" ;;
+esac
+
 case "$env" in
-    "stage")
-        site="adp-devsite-stage" ;;
-    "prod")
-        site="adp-devsite" ;;
+    stage)
+        site="adp-devsite-stage"
+        args='--header "x-content-source-authorization: stage"'
+        ;;
+    prod)
+        site="adp-devsite"
+        args=""
+        ;;
     *)
         fail "Unknown env" ;;
 esac
@@ -68,4 +68,5 @@ esac
 # TODO: may want to only process certain types of files
 # find "${root}" -type f \( -name "*.md" -o -name "*.json" \) -exec echo "{}" \; | while read i; do process $i; done
 
-test_url="https://admin.hlx.page/preview/adobedocs/adp-devsite-stage/main/github-actions-test/redirects.json"
+test_url="../../src/pages/redirects.json"
+process "${test_url}"
