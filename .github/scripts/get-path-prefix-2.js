289db2e3e7bdf01ef8e32a401cdc9f24256ddb73
Melissa Garcia <ens15038@adobe.com>
2025-02-24 15:35:47 -0800

    check file

--- .github/scripts/get-path-prefix-2.js
+++ .github/scripts/get-path-prefix-2.js
@@ -2,15 +2,22 @@
 // cd /Users/melissag/Projects/adp-devsite-github-actions-test/.github/scripts
 // node get-path-prefix-2
 
-async function sdf() {
+const test = async () => {
     console.log('~~ hello 2');
     const url = "https://raw.githubusercontent.com/aemsites/devsite-runtime-connector/refs/heads/main/src/devsite-paths.json";
 
+    const fs = await require('fs');    
+    if (!fs.existsSync('../../src/pages/config.md')) {
+        console.log('file doesnt exist');
+    } else {
+        console.log('file exists')
+    }
+
     const obj = await (await fetch(url)).json();
-    console.log('~~ obj', obj);
+    // console.log('~~ obj', obj);
 };
 
-sdf();
+test();
 
 
   
