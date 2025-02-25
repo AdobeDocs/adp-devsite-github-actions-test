821acf960578ec0d75de5640c7887b3f7fbf2c9d
Melissa Garcia <ens15038@adobe.com>
2025-02-24 16:24:51 -0800

    pathPrefix from gatsby

--- .github/scripts/get-path-prefix-2.js
+++ .github/scripts/get-path-prefix-2.js
@@ -6,15 +6,22 @@ const test = async () => {
     console.log('~~ hello 2');
     const url = "https://raw.githubusercontent.com/aemsites/devsite-runtime-connector/refs/heads/main/src/devsite-paths.json";
 
-    const fs = await require('fs');    
-    if (!fs.existsSync('../../src/pages/config.md')) {
+    const { pathPrefix } = await require('../../gatsby-config.js');
+    console.log('~~ pathPrefix', pathPrefix);
+
+    const configFile = await require('fs');    
+    if (!configFile.existsSync('../../src/pages/config.md')) {
         console.log('file doesnt exist');
     } else {
         console.log('file exists')
     }
 
-    const obj = await (await fetch(url)).json();
+    const devsitePaths = await (await fetch(url)).json();
+
+
     // console.log('~~ obj', obj);
+
+
 };
 
 test();
