00eabc87eed186bd8a262f460c113c9618421477
Melissa Garcia <ens15038@adobe.com>
2025-02-25 22:48:53 -0800

    clean up wip

--- .github/scripts/get-path-prefix.js
+++ .github/scripts/get-path-prefix.js
@@ -2,23 +2,18 @@
 // cd /Users/melissag/Projects/adp-devsite-github-actions-test/.github/scripts
 // node get-path-prefix-2
 
-const devsitePathsUrl = "https://raw.githubusercontent.com/aemsites/devsite-runtime-connector/refs/heads/main/src/devsite-paths.json";
-const root='../../src/pages';
-const configPath = `${root}/config.md`;
-// // TODO - pass repo as an arg to this script. 
-// // e.g. https://github.com/AdobeDocs/adp-devsite-github-actions-test/actions/runs/13468667666/job/37639187537
-const owner = "AdobeDocs";
-const repo = "adp-devsite-github-actions-test";
+const CONFIG_PATH = '../../src/pages/config.md';
+const DEVSITE_PATHS_URL = "https://raw.githubusercontent.com/aemsites/devsite-runtime-connector/refs/heads/main/src/devsite-paths.json";
 
 const getFromConfig = async () => {
     // fail if config.md is missing
     const fs = await require('fs');    
-    if (!fs.existsSync(configPath)) {
+    if (!fs.existsSync(CONFIG_PATH)) {
         return null;
     }
   
     // get pathPrefix from config
-    let config = fs.readFileSync(configPath).toString();
+    let config = fs.readFileSync(CONFIG_PATH).toString();
     const lines = config.split('\n');
     const keyIndex = lines.findIndex(line => line.includes("pathPrefix:"));
     const line = lines.slice(keyIndex + 1).find(line => line.trimStart().startsWith("-"));
@@ -28,23 +23,23 @@ const getFromConfig = async () => {
     return pathPrefixFromConfig;
 }
 
-const getFromDevsitePaths = async () => {
+const getFromDevsitePaths = async ({owner, repo}) => {
   // get pathPrefix from devsite-paths
-  const devsitePaths = await (await fetch(devsitePathsUrl)).json();
+  const devsitePaths = await (await fetch(DEVSITE_PATHS_URL)).json();
   const entry = devsitePaths.find(entry => entry.repo === repo && entry.owner === owner);
   const pathPrefixFromDevsitePaths = entry.pathPrefix;
   return pathPrefixFromDevsitePaths;
 }
 
-const test = async () => {
+const test = async ({owner, repo}) => {
   const fromConfig = await getFromConfig();
   console.log('fromConfig: ', fromConfig);
 
-  const fromDevsitePaths = await getFromDevsitePaths();
+  const fromDevsitePaths = await getFromDevsitePaths({owner, repo});
   console.log('fromDevsitePaths: ', fromDevsitePaths);
 };
 
-test();
+test({owner: "AdobeDocs", repo: "adp-devsite-github-actions-test"});
 
 
 
