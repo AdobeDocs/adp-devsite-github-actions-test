3784e54ded3289a742fe1698d285e247d34c0d1f
Melissa Garcia <ens15038@adobe.com>
2025-02-28 13:42:42 -0800

    feat: publish script

--- .github/scripts/get-path-prefix.js
+++ .github/scripts/get-path-prefix.js
@@ -1,49 +1,59 @@
-// This script retrieves the pathPrefix from the gatsby-config.js file.
+// This script retrieves the pathPrefix from the config.md file.
 // It serves as an example for how to set up external javascript functions
 // outside workflow .yml files when they get too big or complex to keep them inline.
 
 // Documentation for the actions/github-script:
 // https://github.com/actions/github-script#run-a-separate-file
 
+const CONFIG_PATH = `./src/pages/config.md`;
+
 module.exports = async ({ core }) => {
-  const { pathPrefix } = await require('../../gatsby-config.js');
+  const fs = await require('fs');    
+  if (!fs.existsSync(CONFIG_PATH)) {
+    core.setFailed(
+      `The site's config.md file is missing.
+
+      To fix this, either create one in ./src/pages or auto-generate one from the site's gatsby-config.md file by running build navigation.`
+    );
+    return;
+  }
+
+  let string = fs.readFileSync(CONFIG_PATH).toString() ?? "";
+  const lines = string.split('\n');
+
+  // find the " - pathPrefix:" line
+  const keyIndex = lines.findIndex(line => line.includes("pathPrefix:"));
+
+  if (keyIndex < 0) {
+    core.setFailed(
+      `The pathPrefix in the site's config.md file is missing.
+
+      To fix this, open your config.md file, and add it to the config object:
+
+      - pathPrefix:
+      ...`
+    );
+    return;
+  }
+
+  // find the " - %PATH_PREFIX%" line
+  let line = lines.slice(keyIndex + 1)?.find(line => line.trimStart().startsWith("-")) ?? "";
+
+  // remove whitespace at start, remove dash (i.e. first non-whitespace character), and remove whitespace at start and end
+  pathPrefix = line.trimStart().substring(1).trim();
 
   if (!pathPrefix) {
     core.setFailed(
-      `The pathPrefix in the site's gatsby-config.js file is missing.
+      `The pathPrefix in the site's config.md file is missing.
 
-      To fix this, open your gatsby-config.js file, and add it to the config object:
+      To fix this, open your config.md file, and add it to the config object:
 
-      module.exports = {
-        pathPrefix: "/commerce/frontend-core/",
-        ...
-      }`
+      - pathPrefix:
+          - /commerce/frontend-core/
+      ...`
     );
-  } else if (pathPrefix === '/') {
-    core.setFailed(
-      `The pathPrefix in the site's gatsby-config.js file is set to "/". This is not allowed.
-
-      To fix this, change the pathPrefix to include a name that starts and ends with "/":
-
-      pathPrefix: "/commerce/frontend - core/"
-
-      This name identifies the site within the developer.adobe.com domain:
-      https://developer.adobe.com/document-services/<PATH_TO_FILES>.
-      `
-    );
-  } else {
-    if (!pathPrefix.startsWith('/') || !pathPrefix.endsWith('/')) {
-      core.setFailed(
-        `The pathPrefix in the site's gatsby-config.js file does not start or end with "/".
-
-        To fix this, change the pathPrefix to include a name that starts and ends with "/".
-        For example: "/document-services/" or "/commerce/cloud-tools/".
-
-        This is required by convention because of the way we construct site URLs.
-        For example: https://developer.adobe.com + /document-services/ + path/to/files/.
-        `
-      );
-    }
+    return;
   }
+
   core.setOutput('path_prefix', pathPrefix);
 };
