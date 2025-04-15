14f6432a4a1cf81ee1e3e392ac0b3c1287669c2f
Melissa Garcia <ens15038@adobe.com>
2025-04-15 14:08:32 -0700

    set up test files

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,132 +29,29 @@ module.exports = {
         path: '/'
       },
       {
+        title: 'Sentence case',
+        path: '/SentenceCase'
+      },
+      {
         title: 'Guides',
-        path: '/guides/'
-      },
-      {
-        title: 'API Reference',
-        menu: [
-          {
-            title: 'Cat Reference v2.0',
-            description: 'Cat Reporting API',
-            path: '/api/index.md'
-          },
-          {
-            title: 'Cat Reference v1.4',
-            description: 'Cat API Spec',
-            path: '/api/1-4.md'
-          }
-        ]
-      },
-      {
-        title: 'Support',
-        path: '/support/'
-      },
-      {
-        title: 'Test',
-        path: '/test/'
+        path: '/Guides/'
       }
     ],
     subPages: [
       {
         title: 'Get Started',
-        path: '/guides/',
+        path: '/Guides/',
         pages: [
           {
-            title: 'Dummy an OAuth Client',
-            path: '/guides/dummy_oauth_client/'
+            title: 'Screaming snake case',
+            path: '/Guides/SCREAMING_SNAKE_CASE'
           },
           {
-            title: 'Dummy OAuth using POSTMAN',
-            path: '/guides/dummy_using_postman/'
+            title: 'With dot',
+            path: '/Guides/with.dot'
           }
         ]
-      },
-      {
-        title: 'Cat Metrics API',
-        path: '/guides/dummy_metrics_api/'
-      },
-      {
-        title: 'Migrating',
-        path: '/guides/migrating/'
-      },
-      {
-        title: 'Overview',
-        path: '/support/',
-        header: true,
-        pages: [
-          {
-            title: 'Help',
-            path: '/support/'
-          },
-          {
-            title: 'FAQ',
-            path: '/support/FAQ/'
-          },
-          {
-            title: 'How to contribute',
-            path: '/support/contribute/'
-          }
-        ]
-      },
-      {
-        title: 'Community',
-        path: '/support/community/',
-        header: true,
-        pages: [
-          {
-            title: 'Information',
-            path: '/support/community/'
-          }
-        ]
-      },
-      {
-        title: 'Test',
-        path: '/test/',
-        pages: [
-          {
-            title: 'Code',
-            path: '/test/code/'
-          },
-          {
-            title: 'CodeBlock',
-            path: '/test/code-block/'
-          },
-          {
-            title: 'CodeBlock with Picklist',
-            path: '/test/code-block-with-picklist/'
-          },
-          {
-            title: 'CodeBlock without Picklist',
-            path: '/test/code-block-without-picklist/'
-          },
-          {
-            title: 'Code in List',
-            path: '/test/code-in-list/'
-          },
-          {
-            title: 'Code in Table',
-            path: '/test/code-in-table/'
-          },
-          {
-            title: 'InlineAlert',
-            path: '/test/inline-alert/'
-          },
-          {
-            title: 'ListBlock',
-            path: '/test/list-block/'
-          },
-          {
-            title: 'RedoclyAPIBlock',
-            path: '/test/redocly-api-block/'
-          },
-          {
-            title: 'TabsBlock',
-            path: '/test/tabs-block/'
-          }
-        ]
-      },
+      }
     ]
   },
   plugins: [`@adobe/gatsby-theme-aio`]
