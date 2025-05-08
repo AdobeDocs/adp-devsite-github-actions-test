f748338ac4cbb729f6b0a88219c39e92c85e686d
timkim <timk@adobe.com>
2025-05-08 16:52:46 -0700

    finally

--- .github/scripts/deploy.js
+++ .github/scripts/deploy.js
@@ -1,3 +1,12 @@
 module.exports = async ({ core, changes, isStage, isProd }) => {
   core.debug('Inside deploy.js');
+
+  core.debug('changes: ');
+  core.debug(changes);
+
+  core.debug('isStage: ');
+  core.debug(isStage.toLowerCase() === 'true');
+
+  core.debug('isProd: ');
+  core.debug(isProd.toLowerCase() === 'true');
 }
