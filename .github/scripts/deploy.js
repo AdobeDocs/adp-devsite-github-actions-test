03350bb2c7ecec56d88d178df93cadff5dac7f6c
timkim <timk@adobe.com>
2025-05-09 15:23:08 -0700

    Test

--- .github/scripts/deploy.js
+++ .github/scripts/deploy.js
@@ -1,12 +1,21 @@
-module.exports = async ({ core, changes, isStage, isProd }) => {
+module.exports = async ({ core, exec, changes, deletions, isStage, isProd }) => {
   console.log('Inside deploy.js');
+
+  console.log('exec: ');
+  console.log(exec);
 
   console.log('changes: ');
   console.log(changes);
+
+  console.log('deletions: ');
+  console.log(deletions);
 
   console.log('isStage: ');
   console.log(isStage.toLowerCase() === 'true');
 
   console.log('isProd: ');
   console.log(isProd.toLowerCase() === 'true');
+
+  // "https://admin.hlx.page/${OPERATION}/adobedocs/${site}/${code_repo_branch}/${path}"
+
 }
