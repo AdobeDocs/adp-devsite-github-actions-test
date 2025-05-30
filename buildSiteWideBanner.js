1efd8194a203ef47a5442622d9bb360120c0fc8a
BaskarMitrah <mit44186@adobe.com>
2025-05-30 17:08:40 +0530

    update script : buildSideWideBanner

--- buildSiteWideBanner.js
+++ buildSiteWideBanner.js
@@ -4,19 +4,18 @@ const fs = require('node:fs');
 const { siteMetadata } = require('./gatsby-config.js');
 
 try {
-
     let sideWideBanner = ``;
     if (siteMetadata.siteWideBanner) {
         sideWideBanner = {
-            ...JSON.stringify(siteMetadata.siteWideBanner),
+            "data": siteMetadata.siteWideBanner,
             ":type": "sheet"
         };
     }
 
-    let configFilePath = path.resolve(__dirname + '/src/pages/sitewidebanner.json');
-    fs.writeFileSync(configFilePath, sideWideBanner);
-    console.log(`Generated file: ${configFilePath}`);
+    const configFilePath = path.resolve(__dirname, 'src/pages/sitewidebanner.json');
 
+    fs.writeFileSync(configFilePath, JSON.stringify(sideWideBanner));
+    console.log(`Generated file: ${configFilePath}`);
 } catch (err) {
     console.error(err);
 }
