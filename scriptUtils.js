6fd32b3f09b5891a04b6a0fc2d86d820f564f526
Melissa Garcia <ens15038@adobe.com>
2025-06-18 08:39:24 -0700

    refactor: prettier

--- scriptUtils.js
+++ scriptUtils.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('node:fs');
-const { globSync }= require('glob');
+const { globSync } = require('glob');
 
 function getRedirectionsFilePath() {
     const redirectionsFilePath = path.join(__dirname, 'src', 'pages', 'redirects.json');
@@ -9,17 +9,16 @@ function getRedirectionsFilePath() {
 
 function readRedirectionsFile() {
     const redirectionsFilePath = getRedirectionsFilePath();
-    return JSON.parse(fs.readFileSync(redirectionsFilePath)).data; 
+    return JSON.parse(fs.readFileSync(redirectionsFilePath)).data;
 }
 
 function writeRedirectionsFile(data) {
-    let redirectionsData = 
-    {
-        "total" : data.length,
-        "offset": 0,
-        "limit": data.length,
-        "data" : data,
-        ":type": "sheet"
+    let redirectionsData = {
+        total: data.length,
+        offset: 0,
+        limit: data.length,
+        data: data,
+        ':type': 'sheet',
     };
 
     let redirectionsFilePath = getRedirectionsFilePath();
@@ -28,12 +27,11 @@ function writeRedirectionsFile(data) {
 
 function getFiles(fileExtensions) {
     const fileExtensionsPattern = fileExtensions.join('|');
-    return globSync(__dirname + `/src/pages/**/*+(${fileExtensionsPattern})`)
-        .map(f => path.relative(__dirname, f));
+    return globSync(__dirname + `/src/pages/**/*+(${fileExtensionsPattern})`).map((f) => path.relative(__dirname, f));
 }
 
 function getDeployableFiles() {
-    // files types deployed to EDS in process-mds.sh 
+    // files types deployed to EDS in process-mds.sh
     return getFiles(['.md', '.json']);
 }
 
@@ -46,7 +44,7 @@ function removeFileExtension(file) {
     const ext = path.extname(file);
     const end = file.length - base.length;
     const baseWithoutExt = base.substring(0, base.length - ext.length);
-    return `${file.substring(0, end)}${baseWithoutExt}`
+    return `${file.substring(0, end)}${baseWithoutExt}`;
 }
 
 const getFindPatternForMarkdownFiles = (from) => `(\\[[^\\]]*]\\()(/|./)?(${from})(#[^\\()]*)?(\\))`;
@@ -57,7 +55,7 @@ function replaceLinksInFile({ file, linkMap, getFindPattern, getReplacePattern }
     linkMap.forEach((to, from) => {
         const find = getFindPattern(from);
         const replace = getReplacePattern(to);
-        data = data.replaceAll(new RegExp(find, "gm"), replace);
+        data = data.replaceAll(new RegExp(find, 'gm'), replace);
     });
     fs.writeFileSync(file, data, 'utf-8');
 }
@@ -71,5 +69,5 @@ module.exports = {
     getFindPatternForMarkdownFiles,
     getReplacePatternForMarkdownFiles,
     removeFileExtension,
-    replaceLinksInFile
+    replaceLinksInFile,
 };
