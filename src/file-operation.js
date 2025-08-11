5571383dec0424558da94a553c46a5073e39173d
Yuxuan Jiang <jiangy10@rose-hulman.edu>
2025-08-11 08:57:30 -0700

    fix module export

--- src/file-operation.js
+++ src/file-operation.js
@@ -1,6 +1,10 @@
-export function hasMetadata(content){
+function hasMetadata(content){
     if (!content.startsWith('---')) return false;
     const metadataEnd = content.indexOf("---", content.indexOf("---") + 1);
     const metadata = content.slice(3, metadataEnd + 3).trim();
     return metadata.includes("title") || metadata.includes("description") || metadata.includes("keywords");
 }
+
+module.exports = {
+    hasMetadata
+};
